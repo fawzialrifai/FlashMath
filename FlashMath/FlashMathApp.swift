109a0a88be74b47ecf6cc7e9af3b8b18a9c5f17b
Fawzi <fawzialrifai@icloud.com>
2022-05-28 20:16:17 +0300

    Initial Commit

---
+++ FlashMath/FlashMathApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashMathApp.swift
+//  FlashMath
+//
+//  Created by Fawzi Rifai on 28/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct FlashMathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
